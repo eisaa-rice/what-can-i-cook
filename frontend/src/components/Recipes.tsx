157dbd5214fed6388f0351aaa2457b6475761b27
jésus orozco <jesusorozco3690@gmail.com>
2026-07-13 02:42:00 -0400

    recipe ui finished

--- frontend/src/components/Recipes.tsx
+++ frontend/src/components/Recipes.tsx
@@ -3,6 +3,30 @@ import { useState } from "react";
 import type { Appliance, Cookware, Utensil } from "../types/request";
 import type { Recipe, RecipeStep } from "../types/response";
 
+import { X } from "lucide-react";
+
+const RecipeCard = ({
+  name,
+  description,
+  onClick,
+}: {
+  name: string;
+  description: string;
+  onClick: () => void;
+}) => {
+  return (
+    <li
+      className="border border-neutral-300 rounded-sm
+      p-3 m-2 hover:cursor-pointer"
+      onClick={onClick}
+    >
+      <p className="font-bold">{name}</p>
+
+      <p className="text-neutral-700 mt-1.5">{description}</p>
+    </li>
+  );
+};
+
 const List = ({ items }: { items: string[] }) => {
   return (
     <ul className="flex items-center gap-2 flex-wrap">
@@ -39,9 +63,10 @@ const Recipe = ({
 }) => {
   return (
     <li
-      className="flex flex-col gap-4
+      // TODO: fix vertical scroll on thinner screens
+      className="flex flex-col gap-4 overflow-y-auto
       border border-neutral-200 rounded-sm
-      m-4 p-4"
+      m-4 p-4 bg-white"
     >
       <p className="font-bold">{name}</p>
 
@@ -75,36 +100,48 @@ const Recipe = ({
 };
 
 const Recipes = ({ recipes }: { recipes: Recipe[] }) => {
-  const [page, setPage] = useState(0);
+  const [recipe, setRecipe] = useState<number | null>(null);
 
   return (
-    // TODO: maybe show list/grid of recipe previews, and then activate pagination on selection (like a modal)
     <>
-      <ul>
-        <Recipe
-          name={recipes[page].name}
-          description={recipes[page].description}
-          appliances={recipes[page].appliances}
-          cookware={recipes[page].cookware}
-          utensils={recipes[page].utensils}
-          ingredients={recipes[page].ingredients}
-          steps={recipes[page].steps}
-        />
+      <ul className="flex flex-col gap-2">
+        {recipes.map(({ name, description }, idx) => (
+          <RecipeCard
+            key={idx}
+            name={name}
+            description={description}
+            onClick={() => setRecipe(idx)}
+          />
+        ))}
       </ul>
 
-      <div className="flex items-center justify-center gap-12 text-sm">
-        {page > 0 && page < recipes.length && (
-          <button type="button" onClick={() => setPage((prev) => prev - 1)}>
-            Back
+      {recipe !== null && (
+        <div
+          className="w-full h-full absolute flex items-center justify-center
+          bg-black/25"
+        >
+          <button
+            type="button"
+            className="absolute top-2 right-2 h-8 w-8"
+            onClick={() => setRecipe(null)}
+          >
+            <X
+              className="bg-white rounded-sm border border-neutral-200"
+              strokeWidth={1.5}
+            />
           </button>
-        )}
 
-        {page < recipes.length - 1 && (
-          <button type="button" onClick={() => setPage((prev) => prev + 1)}>
-            Next
-          </button>
-        )}
-      </div>
+          <Recipe
+            name={recipes[recipe].name}
+            description={recipes[recipe].description}
+            appliances={recipes[recipe].appliances}
+            cookware={recipes[recipe].cookware}
+            utensils={recipes[recipe].utensils}
+            ingredients={recipes[recipe].ingredients}
+            steps={recipes[recipe].steps}
+          />
+        </div>
+      )}
     </>
   );
 };
